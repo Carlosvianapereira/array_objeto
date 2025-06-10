19f096dc1b823854259d5c05f9bbee172b13000c
CARLOS ALBERLANIO VIANA PEREIRA <carlospereira6662@digitalcollege.internal>
2025-06-10 20:18:58 -0300

    array

---
+++ array.js
@@ -0,0 +1,91 @@
+//Lista de itens dos comentarios
+//1-criar, 
+// let nomes = ["Ana", "Bia", "Carlos", "Pedro", "Fabio"];
+// let carros =[]
+
+// const idades = ['20, 30, 40, 50, 60'];
+
+// let cidadesArgentina = ['Buenos Aires',
+    'Rosario',
+    'Cordoba',
+    'Mendoza',
+    'La Plata',
+    'Mar del Plata',
+    'San Miguel de Tucuman',
+    'Salta',
+    'Santa Fe',
+    'Corrientes',
+    'Posadas',
+    'Bahia Blanca',
+    'Resistencia',
+    'Parana',
+    'Formosa',
+    'San Juan',
+    'Neuquen',
+    'Santa Rosa',
+    'Santa Maria',
+    'Santiago del Estero',
+    'Ushuaia',
+    'Rawson',
+    'Comodoro Rivadavia',
+    'Merlo',
+    'Viedma',
+    'San Rafael',
+    'San Carlos de Bariloche',
+    'San Nicolas',
+    'San Fernando del Valle de Catamarca',
+    'Río Gallegos',
+    'San Luis',
+    'San Luis',
+    'San Luis',
+
+
+//]
+
+//length; retorna o numero com a quantidade de itens da lista.
+//console.log(`Tamanho: ${cidadesArgentina.length}`);
+
+
+//2-limpar, 
+//console.log(idades);
+
+//idades = []
+//console.log(idades);
+
+//3-adicionar elementos,
+//console.log(carros);
+
+//push add elemento no final
+//carros.push("Fusca", 3);
+//console.log(carros);
+
+//4-remover elementos, 
+//pop remove o ultimo elemento
+//shift remove o primeiro elemento
+//console.log(nomes);
+//nomes.pop();
+//console.log(nomes);
+//nomes.shift();
+//console.log(nomes);
+//console.log(nomes[0]);
+// let nomeProprio = `${nomes[1]} ${nomes[2]}`
+// console.log(nomeProprio.toUpperCase());
+
+
+
+
+//unshift add elemento no inicio
+// carros.unshift("Fusca", 3);
+
+
+
+//5-filtrar elementos,
+
+
+
+
+//6-percorrer os itens
+let frutas = ['Banana', 'Maça', 'Pera', 'Uva', 'Laranja', 'kiwi']
+frutas.forEach(function (frutas) {
+    console.log(frutas)
+})
