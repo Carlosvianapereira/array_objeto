cc00e3f6c0741dd046ef12d16a41e9831cff651f
CARLOS ALBERLANIO VIANA PEREIRA <carlospereira6662@digitalcollege.internal>
2025-06-10 21:30:17 -0300

    meu nome objeto

---
+++ meu_objeto.js
@@ -0,0 +1,29 @@
+let curso = {
+    turma: 'Fs-25',
+    nome: 'Programação em JavaScript',
+    horas: 25,
+    preco: 2700.99,
+
+    professor: {
+        nome: 'Angelo',
+        idade: 25,
+        cidade: 'São Paulo',
+        email: 'XXXXXXXXXXXXXXXX',
+    },
+
+    aluno: {
+        nome: 'João',
+        idade: 20,
+        cidade: 'São Paulo',
+        email: 'XXXXXXXXXXXXXX',
+    },
+
+    sala: {
+        nome: 'Sala 1',
+        cadeiras: 30,
+        projetor: true,
+        arCondicionado: true,
+    }
+}
+
+console.log(curso);
